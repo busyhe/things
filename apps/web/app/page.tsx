9ac3d839bfe72d58366bd15f25baac054a8d114c
busyhe <busyhe@qq.com>
2025-06-20 16:43:45 +0800

    feat: init project

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,12 +1,23 @@
-import { Button } from '@workspace/ui/components/button'
+'use client'
+import ThiingsGrid, { type ItemConfig } from '@/components/ThiingsGrid'
+import Image from 'next/image'
 
-export default function Page() {
+const ThiingsIconCell = ({ gridIndex }: ItemConfig) => {
+  const images = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
   return (
-    <div className="flex items-center justify-center min-h-svh">
-      <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Hello World</h1>
-        <Button size="sm">Button</Button>
-      </div>
+    <div className="group absolute inset-5 flex cursor-pointer items-center justify-center text-white">
+      <Image
+        draggable={false}
+        className="h-full w-full transition-transform group-hover:scale-110 active:scale-95"
+        src={`/thiings/${images[gridIndex % images.length]}.png`}
+        alt={`thiings-${images[gridIndex % images.length]}`}
+        width={160}
+        height={160}
+      />
     </div>
   )
+}
+
+export default function Page() {
+  return <ThiingsGrid gridSize={200} renderItem={ThiingsIconCell} initialPosition={{ x: 0, y: 0 }} />
 }
