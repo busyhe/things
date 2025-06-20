b6d3fe1118f71ab2895fa2734a31347b35c445db
busyhe <busyhe@qq.com>
2025-06-20 16:06:16 +0800

    Initial commit

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'all',
+  bracketSpacing: true,
+  trailingComma: 'none',
+  printWidth: 120,
+  tabWidth: 2,
+  useTabs: false,
+  endOfLine: 'auto'
+}
---
+++ apps/web/config/site.ts
@@ -0,0 +1,17 @@
+export const siteConfig = {
+  name: 'Next.js Starter',
+  url: 'https://github.com/busyhe/nextjs-starter',
+  ogImage: 'https://ui.shadcn.com/og.jpg',
+  description: 'A starter template for Next.js 15 with Shadcn UI and Tailwind CSS',
+  links: {
+    twitter: 'https://twitter.com/shadcn',
+    github: 'https://github.com/shadcn-ui/ui'
+  }
+}
+
+export type SiteConfig = typeof siteConfig
+
+export const META_THEME_COLORS = {
+  light: '#ffffff',
+  dark: '#09090b'
+}
